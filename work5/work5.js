3eefd79dd1dd7ce4731d644af9679b53c7de0cbd
Carlyle1905 <1370413410@qq.com>
2019-10-14 13:02:19 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,32 +1,30 @@
-let str = '<table border="1">'
-for (let i=1;i>10;++i){
+str = '<table border="1">'
+for(let i=9;i>0;--i){
     str += '<tr>'
-    for(var j=1;j<10;++j){
-    str += '<td>&nbsp;</td>'
+    for(var j=9;j>0;--j){
+        str +='<td>&nbsp;</td>'
     }
     str +='</tr>'
 }
-str +='</table>'
-document.getElementById('table').innerHTML = str
-
-str = '<table border="2">'
+str += '</table>'
+document.getElementById('table1').innerHTML = str
+str = '<table border="1">'
 for(let i=9;i>0;--i){
     str += '<tr>'
     for(var j=i;j>0;--j){
-        str += '<td>&nbsp;</td>'
+        str +='<td>&nbsp;</td>'
     }
     str +='</tr>'
 }
-str +='</table>'
-document.getElementById('table2').innerHTML=str
-
-str = '<table border="3">'
-for (let i=9;i>0;--1){
+str += '</table>'
+document.getElementById('table2').innerHTML = str
+str = '<table border="1">'
+for(let i=9;i>0;--i){
     str += '<tr>'
     for(var j=i;j>0;--j){
-    str += '<td>'+ j + '*' + i + '=' + (j * i) + '</td>'
+        str +='<td>' + j + '*' + i + '=' + (j*i) + '</td>';
     }
     str +='</tr>'
 }
-str +='</table>'
+str += '</table>'
 document.getElementById('table3').innerHTML = str
