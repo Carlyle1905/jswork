150c7d1a158038bc236cba5e3c9ff7a7398bce4f
Carlyle1905 <1370413410@qq.com>
2019-10-17 09:47:13 +0800

    work5

---
+++ work6/pai.js
@@ -0,0 +1,30 @@
+function calculate(){
+    let num = document.getElementById('num').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById('num').value = pi(num)
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i=1;i<=n;++i){
+    sum
+    }
+    return sum
+}
+function oddFactorial(n){
+    let sum = 1
+    for(let i=1;1+2n;++i){
+        sum
+    }
+return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let divident = factorial()
+        let divisor = oddFactorial()
+        sum 
+    }
+    return sum
+}
