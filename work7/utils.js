0969cf57e33d4447c3ff1a8b3980069f135f7d45
Carlyle1905 <1370413410@qq.com>
2019-10-21 18:02:57 +0800

    work7slide

--- work7/utils.js
+++ work7/utils.js
@@ -3,7 +3,7 @@ function bubbleSort(str){
     for (let i = 0;i<arr.length - 1;i++){
         for(let j = 0;j<arr.length - i - 1;j++){
             if (arr[j]>arr[j+1]){
-                [arr[j],arr[j + 1]] = [arr[j+1],arr[j]]
+                [arr[j],arr[j + 1]] = [arr[j + 1],arr[j]]
             }
         }
     }
@@ -13,7 +13,7 @@ function insertSort(str){
     let arr = str.split(',')
     for (let i = 1;i < arr.length; i++){
         for (let j = i;j>0;j--){
-            if (arr[j + 1]>arr[j]){
+            if (arr[j - 1]>arr[j]){
                 [arr[j-1],arr[j]] = [arr[j],arr[j - 1]]
             }
         }
