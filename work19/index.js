692bd486d3f8a8fbe122b743597b2eb34f6a6670
Carlyle1905 <1370413410@qq.com>
2019-12-26 17:52:59 +0800

    work19

--- work19/index.js
+++ work19/index.js
@@ -1,32 +1,35 @@
-let ws
+let ws 
 function connect() {
-    let server = document.getElementById('server').nodeValue
+    let server = document.getElementById('server').value
     ws = new WebSocket(server);
-    ws.onopen = function(){
+    ws.onopen = function () {
         document.getElementById('conn').disabled='disable';
         document.getElementById('disconn').disabled='';
-        let nickname = document.getElementById('nickname').Value
+        let nickname=document.getElementById('nickname').value
         if(nickname){
             ws.send('nickname|'+nickname)
         }
-    }
-    ws.onclose = function(){
+
+      }
+      ws.onclose = function () {
         document.getElementById('conn').disable='';
-        document.getElementById('disconn').disabled='disabled';
+        document.getElementById('disconn').disable='disable';
+     }
+      ws.onmessage=function(event) {
+          let board = document.getElementById('board')
+          let newmsg = document.createElement('div')
+          console.log(event.data)
+          newmsg.innerHTML=event.data
+          board.appendChild(newmsg)
+          board.scrollTop = board.scrollHeight;
+        }
+  }
+
+  function disconnect() {
+      ws.close()
     }
-    ws.onmessage=function(event){
-        let board = document.getElementById('board')
-        let newmsg = doucument.createElement('div')
-        console.log(event.data)
-        newmsg.innerHTML = event.data
-        board.appendChild(newmsg)
-        board.scrollTop = board.scrollHeight;
+
+  function send() {
+      let msg = document.getElementById('content').value
+      ws.send(msg)
     }
-}
-function disconnect(){
-    ws.close()
-}
-function send(){
-    let msg = document.getElementById('content').value
-    ws.send(msg)
-}
--- work19/index1.js
+++ work19/index1.js
@@ -1,9 +1,9 @@
-document.getElementById('upload').onclick = function (){
+document.getElementById('upload').onclick = function(){
     var form = document.getElementById('form');
     var fd = new FormData(form);
-    var bar = document.getElementById('bar');//进度条
-    var per = document.getElementById('per');//百分比
-    var down = document.getElementById('download');//地址
+    var bar = document.getElementById('bar');
+    var per = document.getElementById('per');
+    var down = document.getElementById('download');
     var xhr = new XMLHttpRequest();
     xhr.upload.onprogress = function(e){
         var num = Math.floor(e.loaded / e.total * 100);
@@ -12,16 +12,18 @@ document.getElementById('upload').onclick = function (){
     };
     xhr.onreadystatechange = function(){
         if(xhr.readyState === XMLHttpRequest.DONE){
-            if(xhr.status < 200 || xhr,status >= 300&& xhr.status !== 304){
-                throw new Error('upload failed,server error');
+            if(xhr.status < 200 || xhr.status >= 300 && xhr.status !== 304){
+                throw new Error('文件上传失败，服务器状态异常');
             }
             var name = xhr.responseText;
             if(name == ''){
-                throw new error('file save failed');
+                throw new Error('服务器保存文件失败');
             }
-            let reg = /^http(s)?:\/\/(.*?)\//
+            let reg=/^http(s)?:\/\/(.*?)\//
             let downurl = xhr.responseURL.match(reg)[0]+name.slice(2,name.length-1)
-            down.innerHTML = 'upload success'
+            down.innerHTML = `文件上传成功。<a href=${downurl}>下载文件${downurl}</a>`;
         }
-    }
-}
+    };
+    xhr.open('POST', 'http://localhost:8080/upload');
+    xhr.send(fd);
+};
